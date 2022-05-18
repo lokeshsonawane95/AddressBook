3bee508a938ccbbf60b518e1f2f7aa3fbe281bbe
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-18 14:49:33 +0530

    Added to CreateContact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System;
+namespace AddressBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\t\tWelcome to Address Book Program");
+            int choice;
+            do
+            {
+                Console.WriteLine("\n1. Adding contacts details");
+                
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice : ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Contact details are as shown below");
+                        //Adding contact information directly through object
+                        Details details = new Details("Lokesh", "Sonawane", "Warje", "Pune", "Maharashtra", "lokesh@gmail.com", 411058, 919932156480);
+                        details.DisplayDetails();
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Enter correct choice");
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
