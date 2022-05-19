85c8d9fcca8cb3356c88ffed668470b5a0088558
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-19 21:24:02 +0530

    Added to AddMultipleAddressBooks

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -14,6 +14,7 @@ namespace AddressBook
                 Console.WriteLine("3. Edit added contact");
                 Console.WriteLine("4. Delete added person contact using name");
                 Console.WriteLine("5. Add multitple person");
+                Console.WriteLine("6. Add multiple Address Books");
                 Console.WriteLine("0. Exit");
                 Console.Write("Enter your choice : ");
                 choice = Convert.ToInt32(Console.ReadLine());
@@ -117,6 +118,68 @@ namespace AddressBook
                         else
                         {
                             Console.WriteLine("No contact details available for deletion");
+                        }
+                        break;
+                    case 6:
+                        Console.Write("How many Address Books do you need : ");
+                        int need = Convert.ToInt32(Console.ReadLine());
+                        MultipleAddressBook person6 = new MultipleAddressBook(need);
+                    GoAgain:
+                        person6.DisplayAllAddressBooks();
+                        person6.AddingMultipleAddressBooks();
+                        person6.DisplayAllAddressBooks();
+                        person6.AccessingAddressBook();
+
+                    Add1:
+                        Console.Write("You want to enter details ? ( Press 1 for Yes / OtherNumber for No) : ");
+                        int c6 = Convert.ToInt32(Console.ReadLine());
+                        if (c6 == 1)
+                        {
+                            person6.AddingContactDetails();
+                            person6.DisplayDetails();
+                            goto Add1;
+                        }
+
+                    //Asking user if he/she wanted to edit the contact details or not
+                    Edit1:
+                        if (person6.detailsList[person6.addressBookIndex].Count > 0)
+                        {
+                            Console.Write("Edit contact details using name ? ( Press 1 for Yes / OtherNumber for No) : ");
+                            int choice6 = Convert.ToInt32(Console.ReadLine());
+                            if (choice6 == 1)
+                            {
+                                person6.EditContactDetails();
+                                person6.DisplayDetails();
+                                goto Edit1;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No contact details available to edit");
+                        }
+
+                    //Asking user if he/she wanted to delete the contact details or not
+                    Delete1:
+                        if (person6.detailsList[person6.addressBookIndex].Count > 0)
+                        {
+                            Console.Write("Delete person using person name ? ( Press 1 for Yes / OtherNumber for No) : ");
+                            int choice62 = Convert.ToInt32(Console.ReadLine());
+                            if (choice62 == 1)
+                            {
+                                person6.DeleteContactDetails();
+                                person6.DisplayDetails();
+                                goto Delete1;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No contact details available for deletion");
+                        }
+                        Console.WriteLine("Want to choose Address Book again ? ( Press 1 for Yes / OtherNumber for No) : ");
+                        int start = Convert.ToInt32(Console.ReadLine());
+                        if (start == 1)
+                        {
+                            goto GoAgain;
                         }
                         break;
                     default:
